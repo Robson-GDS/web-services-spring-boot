67463d94253d501568c12f31edfb49984dd8c429
Robson <robson.080490@gmail.com>
2024-02-15 08:28:54 -0300

    criando service e resource findAll user.

--- src/main/java/com/springbootcourse/course/resources/UserResource.java
+++ src/main/java/com/springbootcourse/course/resources/UserResource.java
@@ -1,17 +1,24 @@
 package com.springbootcourse.course.resources;
 
 import com.springbootcourse.course.entities.User;
+import com.springbootcourse.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Robson", "robson@email.com", "45454545", "123");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = service.findAll();
+        return ResponseEntity.ok().body(users);
     }
 }
