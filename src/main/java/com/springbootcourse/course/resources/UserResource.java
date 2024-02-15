61ffcca96f6038cf2e7b63aa0bf8700e9ae9558a
Robson <robson.080490@gmail.com>
2024-02-15 08:40:54 -0300

    criando o endpoint findById.

--- src/main/java/com/springbootcourse/course/resources/UserResource.java
+++ src/main/java/com/springbootcourse/course/resources/UserResource.java
@@ -5,10 +5,12 @@ import com.springbootcourse.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,5 +22,11 @@ public class UserResource {
     public ResponseEntity<List<User>> findAll() {
         List<User> users = service.findAll();
         return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
---
+++ src/main/java/com/springbootcourse/course/services/UserService.java
@@ -0,0 +1,24 @@
+package com.springbootcourse.course.services;
+
+import com.springbootcourse.course.entities.User;
+import com.springbootcourse.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> user = repository.findById(id);
+        return  user.get();
+    }
+}
