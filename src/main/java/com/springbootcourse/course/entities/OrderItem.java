c817bf5fd6d6baba7038d53cdf0185f9fed630d1
Robson <robson.080490@gmail.com>
2024-02-29 09:34:55 -0300

    criando entidade Entidade OrderItem.

---
+++ src/main/java/com/springbootcourse/course/entities/OrderItem.java
@@ -0,0 +1,44 @@
+package com.springbootcourse.course.entities;
+
+import com.springbootcourse.course.entities.pk.OrderItemPk;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "order_item")
+@EqualsAndHashCode(of = "id")
+public class OrderItem {
+    @EmbeddedId
+    private OrderItemPk id;
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+}
